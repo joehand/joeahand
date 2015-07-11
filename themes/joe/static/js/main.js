47a56c2ed007b3fce1b32366efbd42bdb140a1fc
Joe Hand <joe.a.hand@gmail.com>
2015-07-10 21:52:40 -0600

    theme updates

---
+++ themes/joe/static/js/main.js
@@ -0,0 +1,40 @@
+console.log(steps_data);
+
+
+
+
+
+var data = {
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    series: [
+    [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8],
+    [3, 2, 9, 5, 4, 6, 4, 6, 7, 8, 7, 4]
+  ]
+};
+
+var options = {
+  seriesBarDistance: 15
+};
+
+var responsiveOptions = [
+  ['screen and (min-width: 641px) and (max-width: 1024px)', {
+    seriesBarDistance: 10,
+    axisX: {
+      labelInterpolationFnc: function (value) {
+        return value;
+      }
+    }
+  }],
+  ['screen and (max-width: 640px)', {
+    seriesBarDistance: 5,
+    axisX: {
+      labelInterpolationFnc: function (value) {
+        return value[0];
+      }
+    }
+  }]
+];
+
+new Chartist.Bar('#coffee', coffee_data, options, responsiveOptions);
+
+var chart = new Chartist.Bar('#steps', steps_data, options, responsiveOptions);
