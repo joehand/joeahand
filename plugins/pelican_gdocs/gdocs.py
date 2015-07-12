925840d23e4865608bf9d8974c1c989062dfe29e
Joe Hand <joe.a.hand@gmail.com>
2015-07-11 23:15:23 -0600

    update charts and templates

--- plugins/pelican_gdocs/gdocs.py
+++ plugins/pelican_gdocs/gdocs.py
@@ -19,10 +19,12 @@
 # SOFTWARE.
 
 import csv
+from datetime import datetime, date
 import logging
 from operator import itemgetter
-
 import requests
+import time
+
 
 from pelican import signals
 
@@ -30,6 +32,11 @@ from pelican import signals
 logger = logging.getLogger(__name__)
 
 class Gdocs_Sheet(object):
+    """
+    Google Docs
+    Init downloads from public html and returns text content
+    Process creates list of data w/ a dict per row
+    """
 
     def __init__(self, gen, url, name='default'):
         self.content = None
@@ -39,9 +46,6 @@ class Gdocs_Sheet(object):
         try:
             r = requests.get(url)
             c = r.text
-            print('\n')
-            print(r.encoding)
-            print('\n')
             c = ''.join([i if ord(i) < 128 else ' ' for i in c]) # replace ascii chars
         except:
             logger.warning("unable to open {0}".format(url))
@@ -56,6 +60,46 @@ class Gdocs_Sheet(object):
         data = list(csv.DictReader(lines[1:], fieldnames=header))
         return data
 
+class Gdocs_Coffee(Gdocs_Sheet):
+    """
+    Special processing for coffee sheet
+    """
+    def __init__(self, *args, **kwargs):
+        self.max_cups = 0
+        self.intervals = ((5,7),(7,9),(9,11),(11,13),(13,15),(15,17))
+        super(Gdocs_Coffee, self).__init__(*args, **kwargs)
+
+    def process(self):
+        data = super(Gdocs_Coffee, self).process()
+        clean_data = {}
+        for item in data:
+            date_fmt = "%B %d, %Y at %I:%M%p"
+            date_time = time.strptime(item['Date_Time'], date_fmt)
+            delta = date.today() - datetime.strptime(item['Date_Time'], date_fmt).date()
+            days_ago = delta.days
+            hour = (float(time.strftime("%H", date_time)) +
+                        float(time.strftime("%M", date_time))/60)
+
+            clean_data.setdefault(days_ago,[0 for i in self.intervals])
+            for i,interval in enumerate(self.intervals):
+                if interval[0] < hour < interval[1]:
+                    clean_data[days_ago][i] += 1
+                    if clean_data[days_ago][i] > self.max_cups:
+                        self.max_cups = clean_data[days_ago][i]
+                    continue
+        # print(clean_data)
+        no_data = []
+        for day in range(0,31):
+            if day not in clean_data:
+                clean_data[day] = [0 for i in self.intervals]
+                no_data.append(day)
+        return {
+            'max' : self.max_cups,
+            'intervals': self.intervals,
+            'data': clean_data,
+            'no_data':no_data
+        }
+
 
 def initialize(gen):
     if not 'GDOCS' in gen.settings.keys():
@@ -63,8 +107,14 @@ def initialize(gen):
     else:
         gen.plugin_sheets = []
         for sheet in gen.settings['GDOCS']:
-            gen.plugin_sheets.append(
-                    (sheet['name'],Gdocs_Sheet(gen, sheet['url'],name=sheet['name']))
+            name = sheet['name']
+            if name == 'coffee':
+                gen.plugin_sheets.append(
+                    (name,Gdocs_Coffee(gen, sheet['url'],name=name))
+                )
+            else:
+                gen.plugin_sheets.append(
+                    (name,Gdocs_Sheet(gen, sheet['url'],name=name))
                 )
 
 def fetch(gen, metadata):
